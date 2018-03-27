28af18143e7b0c72cbac2582990b37d42626a99b
Eenae <alexey@mixbytes.io>
2018-03-27 12:07:02 +0300

    token freezing test

--- test/SmartzTokenTest.js
+++ test/SmartzTokenTest.js
@@ -4,6 +4,7 @@ import {tokenUTest} from './utest/Token';
 import {l} from './helpers/debug';
 import expectThrow from './helpers/expectThrow';
 import {assertBigNumberEqual} from './helpers/asserts';
+import {withRollback} from './helpers/EVMSnapshots';
 
 const SmartzToken = artifacts.require('SmartzToken.sol');
 const SmartzTokenTestHelper = artifacts.require('SmartzTokenTestHelper.sol');
@@ -84,7 +85,7 @@ contract('SmartzTokenTest', function(accounts) {
 
         // early investment
         await token.frozenTransfer(investor2, SMTZ(40), 1560000000, true, {from: owner1});
-        KYC.setKYCPassed(investor2);
+        await KYC.setKYCPassed(investor2);
         assertBigNumberEqual(await token.balanceOf(investor2), SMTZ(40));
         assertBigNumberEqual(await token.availableBalanceOf(investor2), SMTZ(0));
         await expectThrow(token.transfer(nobody, SMTZ(1), {from: investor2}));  // can't sell yet
@@ -96,7 +97,7 @@ contract('SmartzTokenTest', function(accounts) {
 
         // minting by ico to an investor
         await token.frozenTransfer(investor1, SMTZ(20), 1550000000, true, {from: ico});
-        KYC.setKYCPassed(investor1);
+        await KYC.setKYCPassed(investor1);
         assertBigNumberEqual(await token.balanceOf(investor1), SMTZ(20));
         assertBigNumberEqual(await token.availableBalanceOf(investor1), SMTZ(0));
 
@@ -146,5 +147,225 @@ contract('SmartzTokenTest', function(accounts) {
         for (const role of [owner1, owner2, investor1, investor2, investor3, ico, nobody])
             sum = sum.add(await token.availableBalanceOf(role));
         assertBigNumberEqual(sum, SMTZ(150e6));
+    });
+
+    it('test token freezing', async function() {
+        const owner1 = accounts[0];
+        const holder1 = accounts[1];
+        const holder2 = accounts[2];
+        const holder3 = accounts[3];
+
+        const token = await SmartzTokenTestHelper.new([owner1], 1, {from: owner1});
+        await token.setSale(owner1, true, {from: owner1});
+        await token.setTime(1600000001);
+        await token.approve(owner1, SMTZ(1e6), {from: holder1});
+
+        const KYC = await TestKYCProvider.new();
+        await token.setKYCProvider(KYC.address, {from: owner1});
+
+        // holder1 <- 5 (thaw = 1600000005, KYC = false)
+        await token.frozenTransfer(holder1, SMTZ(5), 1600000005, false, {from: owner1});
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(5));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+        // holder1 <- 7 (thaw = 1600000005, KYC = true)
+        await token.frozenTransfer(holder1, SMTZ(7), 1600000005, true, {from: owner1});
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(12));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+        await expectThrow(token.transfer(holder2, SMTZ(1), {from: holder1}));
+
+        await withRollback(async () => {
+            // no such tokens
+            await expectThrow(token.frozenTransferFrom(holder1, owner1, SMTZ(1), 1700000000, false, {from: owner1}));
+
+            // not enough tokens which are (thaw = 1600000005, KYC = false)
+            await expectThrow(token.frozenTransferFrom(holder1, owner1, SMTZ(6), 1600000005, false, {from: owner1}));
+
+            // not a sale
+            await expectThrow(token.frozenTransferFrom(holder1, holder3, SMTZ(1), 1600000005, true, {from: owner1}));
+            await expectThrow(token.frozenTransferFrom(holder1, owner1, SMTZ(1), 1600000005, true, {from: holder1}));
+            await expectThrow(token.frozenTransferFrom(holder1, holder3, SMTZ(1), 1600000005, true, {from: holder1}));
+
+            await expectThrow(token.frozenTransfer(holder3, SMTZ(1), 1600000005, false, {from: holder1}));
+            await expectThrow(token.frozenTransfer(holder3, SMTZ(1), 1600000005, true, {from: holder1}));
+            await expectThrow(token.frozenTransfer(holder3, SMTZ(1), 1600000010, false, {from: holder1}));
+            await expectThrow(token.frozenTransfer(holder3, SMTZ(1), 1600000010, true, {from: holder1}));
+
+            // there are enough tokens which are (thaw = 1600000005, KYC = true)
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(6), 1600000005, true, {from: owner1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(6));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+        });
+
+
+        await token.setTime(1600000002);
+
+        // holder1 <- 1 (thaw = 1600000005, KYC = false)
+        await token.frozenTransfer(holder1, SMTZ(1), 1600000005, false, {from: owner1});
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(13));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+        // holder1 <- 2 (thaw = 1600000005, KYC = true)
+        await token.frozenTransfer(holder1, SMTZ(2), 1600000005, true, {from: owner1});
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(15));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+        // checking various frozen transferFrom calls
+        await withRollback(async () => {
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(6), 1600000005, false, {from: owner1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(9));
+        });
+
+        await withRollback(async () => {
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(2), 1600000005, false, {from: owner1});
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(4), 1600000005, false, {from: owner1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(9));
+        });
+
+        await withRollback(async () => {
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(9), 1600000005, true, {from: owner1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(6));
+        });
+
+        await withRollback(async () => {
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(3), 1600000005, true, {from: owner1});
+            await token.frozenTransferFrom(holder1, owner1, SMTZ(6), 1600000005, true, {from: owner1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(6));
+        });
+
+        await expectThrow(token.transfer(holder2, SMTZ(1), {from: holder1}));
+
+        // some tokens are unfrozen
+        await token.setTime(1600000006);
+
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(15));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(6));
+
+        await withRollback(async () => {
+            await expectThrow(token.transfer(holder2, SMTZ(100), {from: holder1}));
+
+            // entire cell could be transferred
+            await token.transfer(holder2, SMTZ(6), {from: holder1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(9));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+            assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(6));
+            assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(6));
+        });
+
+        await withRollback(async () => {
+            // transfer of thaw + regular tokens
+            await token.transfer(holder1, SMTZ(10), {from: owner1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(25));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(16));
+
+            await expectThrow(token.transfer(holder2, SMTZ(100), {from: holder1}));
+
+            await token.transfer(holder2, SMTZ(13), {from: holder1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(12));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(3));
+
+            assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(13));
+            assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(13));
+        });
+
+        await withRollback(async () => {
+            // transfer of regular tokens (not touching frozen tokens)
+            await token.transfer(holder1, SMTZ(10), {from: owner1});
+
+            await token.transfer(holder2, SMTZ(2), {from: holder1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(23));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(14));
+
+            assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(2));
+            assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(2));
+        });
+
+        await withRollback(async () => {
+            // multiple transfers
+            await token.transfer(holder2, SMTZ(2), {from: holder1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(13));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(4));
+
+            assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(2));
+            assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(2));
+
+            await expectThrow(token.transfer(holder2, SMTZ(5), {from: holder1}));
+            await token.transfer(holder2, SMTZ(4), {from: holder1});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(9));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+            assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(6));
+            assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(6));
+        });
+
+        await expectThrow(token.transfer(holder2, SMTZ(100), {from: holder1}));
+
+        // transferFrom
+        await token.approve(holder3, SMTZ(1e6), {from: holder1});
+
+        await withRollback(async () => {
+            await expectThrow(token.transferFrom(holder1, holder2, SMTZ(100), {from: holder3}));
+
+            // entire cell could be transferred
+            await token.transferFrom(holder1, holder2, SMTZ(6), {from: holder3});
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(9));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+            assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(6));
+            assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(6));
+        });
+
+        await token.transferFrom(holder1, holder2, SMTZ(2), {from: holder3});
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(13));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(4));
+
+        assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(2));
+        assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(2));
+
+        await expectThrow(token.transferFrom(holder1, holder2, SMTZ(5), {from: holder3}));
+        await token.transferFrom(holder1, holder2, SMTZ(2), {from: holder3});
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(11));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(2));
+
+        assertBigNumberEqual(await token.balanceOf(holder2), SMTZ(4));
+        assertBigNumberEqual(await token.availableBalanceOf(holder2), SMTZ(4));
+
+        // further unfreezing
+        await KYC.setKYCPassed(holder1);
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(11));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(11));
+        await expectThrow(token.transfer(holder3, SMTZ(100), {from: holder1}));
+
+        await withRollback(async () => {
+            // single transfer
+            await token.transfer(holder3, SMTZ(11), {from: holder1});
+
+            assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(0));
+            assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+            assertBigNumberEqual(await token.balanceOf(holder3), SMTZ(11));
+            assertBigNumberEqual(await token.availableBalanceOf(holder3), SMTZ(11));
+        });
+
+        // multiple transfers
+        await expectThrow(token.transfer(holder3, SMTZ(12), {from: holder1}));
+        await token.transfer(holder3, SMTZ(9), {from: holder1});
+
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(2));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(2));
+
+        assertBigNumberEqual(await token.balanceOf(holder3), SMTZ(9));
+        assertBigNumberEqual(await token.availableBalanceOf(holder3), SMTZ(9));
+
+        await expectThrow(token.transfer(holder3, SMTZ(3), {from: holder1}));
+        await token.transfer(holder3, SMTZ(2), {from: holder1});
+
+        assertBigNumberEqual(await token.balanceOf(holder1), SMTZ(0));
+        assertBigNumberEqual(await token.availableBalanceOf(holder1), SMTZ(0));
+
+        assertBigNumberEqual(await token.balanceOf(holder3), SMTZ(11));
+        assertBigNumberEqual(await token.availableBalanceOf(holder3), SMTZ(11));
     });
 });
